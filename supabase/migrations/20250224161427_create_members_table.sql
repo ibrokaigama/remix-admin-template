199116405a65e04e4f4650322e883fbc9fd2cd81
ibrokaigama <ibrokaigama@gmail.com>
2025-10-26 06:05:43 +0100

    Update 20250224161427_create_members_table.sql

--- supabase/migrations/20250224161427_create_members_table.sql
+++ supabase/migrations/20250224161427_create_members_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE members (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  avatar_url TEXT NOT NULL,
-  name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  location TEXT NOT NULL
+ 
+  name ibrahim kaigama 
+  email kaigamaibrahim66@gmail.com
+  location jimeta yola 
 );
